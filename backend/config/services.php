90b6a1b9092edbbc297161fd2d8a63c5adaacff9
Osama Fayez <eng_osamafayez@hotmail.com>
2026-07-18 18:49:09 +0300

    Add Meta service configuration

--- backend/config/services.php
+++ backend/config/services.php
@@ -34,5 +34,10 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+	'meta' => [
+    'app_id' => env('META_APP_ID'),
+    'app_secret' => env('META_APP_SECRET'),
+    'redirect_uri' => env('META_REDIRECT_URI'),
+    ],
 
 ];
